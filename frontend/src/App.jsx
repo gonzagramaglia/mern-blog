f54db7059be4ed278e6ced0fcde994c74a038695
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-16 14:43:17 -0300

    Header: useLocation hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Navbar, TextInput, Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const path = useLocation().pathname;
   return (
     <>
       <Navbar className="border-b-2">
@@ -28,6 +31,32 @@ const Header = () => {
         <Button color="gray" className="lg:hidden" pill>
           <AiOutlineSearch />
         </Button>
+        <div className="flex gap-2 md:order-2">
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to="/sign-in">
+            <Button color="indigo"> Sign In </Button>
+          </Link>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"}>
+            <Link to="/" as={"div"}>
+              Home
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"}>
+            <Link to="/about" as={"div"}>
+              About
+            </Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"}>
+            <Link to="/projects" as={"div"}>
+              Projects
+            </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
