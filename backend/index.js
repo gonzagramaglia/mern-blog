2e0197dda0dcb8dba4f2a87f5c0c69dd42bc28b5
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-16 16:18:10 -0300

    connecting to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
