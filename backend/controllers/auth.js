0afa198e7f01020f4847b54ba92e4ca6cf5d0a0f
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-17 16:43:34 -0300

    backend: signin page. encryptation and cookies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { email, username, password } = req.body;
@@ -27,5 +28,39 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     return next(err);
     // previously => return res.status(500).json({ message: err.message }
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required."));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found."));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password."));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json("Signin successful.");
+  } catch (err) {
+    next(err);
   }
 };
