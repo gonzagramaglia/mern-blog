7648eb7c2c990377cee99e8cfcb7d30dde04c2cf
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-23 15:06:24 -0300

    frontend: getting user's posts

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -33,7 +33,7 @@ export const getPosts = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
-    const posts = await Post.find(
+    const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
       ...(req.query.slug && { slug: req.query.slug }),
@@ -43,11 +43,9 @@ export const getPosts = async (req, res, next) => {
           { title: { $regex: req.query.searchTerm, $options: "i" } },
           { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
-      })
-    )
-      .sort({
-        updatedAt: sortDirection,
-      })
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
@@ -61,7 +59,7 @@ export const getPosts = async (req, res, next) => {
       now.getDate()
     );
 
-    const lastMonthPosts = Post.countDocuments({
+    const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
 
@@ -70,7 +68,7 @@ export const getPosts = async (req, res, next) => {
       totalPosts,
       lastMonthPosts,
     });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    next(error);
   }
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 import flowbite from "flowbite/plugin";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -10,5 +11,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [flowbite],
+  plugins: [flowbite, tailwindScrollbar],
 };
