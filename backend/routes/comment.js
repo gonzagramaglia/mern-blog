5a30d8af860584be7b69ebff88acbb0bc9fc8017
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-27 02:16:17 -0300

    fullstack: liking comments

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createComment, getPostComments } from "../controllers/comment.js";
+import {
+  createComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createComment);
 router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyUser, likeComment);
 
 export default router;
--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,9 +1,11 @@
 import moment from "moment";
 import { useEffect, useState } from "react";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Comments = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
   const [user, setUser] = useState({});
-  console.log(user);
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -18,6 +20,7 @@ const Comments = ({ comment }) => {
     };
     getUser();
   }, [comment]);
+
   return (
     <>
       <div className="flex p-4 border-b dark:border-gray-600 text-sm">
@@ -39,6 +42,25 @@ const Comments = ({ comment }) => {
           </div>
           <div>
             <p className="text-gray-600 pb-2">{comment.content}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+              <button
+                type="button"
+                onClick={() => onLike(comment._id)}
+                className={`text-gray-400 hover:text-blue-500 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
+                }`}
+              >
+                <FaThumbsUp className="text-sm" />
+              </button>
+              <p className="text-gray-400">
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "like" : "likes")}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -46,4 +68,4 @@ const Comments = ({ comment }) => {
   );
 };
 
-export default Comments;
+export default Comment;
